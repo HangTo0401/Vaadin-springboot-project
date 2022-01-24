bc7df095f3a70ccb566670582c1cccab9c5bbde0
HangTo <tominhhang1996@gmail.com>
2022-01-24 23:13:55 +0700

    Init vaadin-springboot project version 1.0

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import com.vaadin.flow.spring.annotation.EnableVaadin;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.example.demo")
+@EnableVaadin
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
