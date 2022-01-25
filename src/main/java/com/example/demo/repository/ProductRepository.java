871487af8e4c04c4d8868b58645211a2dc4dc2af
HangTo <tominhhang1996@gmail.com>
2022-01-25 14:54:17 +0700

    Create ProductRepo

---
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repository;
+
+import com.example.demo.entity.Product;
+import com.example.demo.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+//    List<Product> findByNameStartsWithIgnoreCase(String name);
+}
