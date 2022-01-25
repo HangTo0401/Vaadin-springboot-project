03164e810b4a16540d5706eb40cc82c317fb9ffc
HangTo <tominhhang1996@gmail.com>
2022-01-25 15:15:28 +0700

    Implement Supplier

--- src/main/java/com/example/demo/entity/Supplier.java
+++ src/main/java/com/example/demo/entity/Supplier.java
@@ -2,21 +2,44 @@ package com.example.demo.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity
+@Table(name = "supplier")
 @Data
 public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private long id;
+
     private String name;
+
+    @Column(name="firstname")
+    private String firstname;
+
+    @Column(name="lastname")
+    private String lastname;
+
+    @Column(name="email")
     private String email;
+
+    @Column(name="address")
     private String address;
-    private Date dayOfBirth;
+
+    @Column(name="date_of_birth")
+    private Date dateOfBirth;
+
+    @Column(name="phone_number")
     private String phoneNumber;
 
-    public Supplier(long id, String name, Date dayOfBirth, String email, String phoneNumber, String address) {
+    public Supplier(long id, String firstname, String lastname, Date dateOfBirth, String email, String phoneNumber, String address) {
         this.id = id;
-        this.name = name;
-        this.dayOfBirth = dayOfBirth;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.name = firstname + " " + lastname;
+        this.dateOfBirth = dateOfBirth;
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.address = address;
