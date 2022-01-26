a95e755e5ed0f70a965f2bf06517b2f3f6cd2041
HangTo <tominhhang1996@gmail.com>
2022-01-26 11:49:14 +0700

    Implement Search filter

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -2,13 +2,19 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 // This will be AUTO IMPLEMENTED CRUD by Spring into a Bean called ProductRepository
 @Repository
 @Transactional
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query("select p from Product p " +
+            "where lower(p.firstname) like lower(concat('%', :searchTerm, '%')) " +
+            "or lower(p.lastname) like lower(concat('%', :searchTerm, '%'))")
+    List<Product> search(@Param("searchTerm") String searchTerm);
 }
--- src/main/java/com/example/demo/repository/SupplierRepository.java
+++ src/main/java/com/example/demo/repository/SupplierRepository.java
@@ -1,7 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Product;
 import com.example.demo.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -12,4 +15,8 @@ import java.util.List;
 @Transactional
 public interface SupplierRepository extends JpaRepository <Supplier, Long> {
     List<Supplier> findByNameStartsWithIgnoreCase(String name);
+    @Query("select s from Supplier s " +
+            "where lower(s.firstname) like lower(concat('%', :searchTerm, '%')) " +
+            "or lower(s.lastname) like lower(concat('%', :searchTerm, '%'))")
+    List<Supplier> search(@Param("searchTerm") String searchTerm);
 }
