2fa973f40cb206632f1190ac1f641f434d8dabb7
HangTo <tominhhang1996@gmail.com>
2022-01-26 20:54:46 +0700

    Create DTO layer

---
+++ src/main/java/com/example/demo/dto/ProductDTO.java
@@ -0,0 +1,29 @@
+package com.example.demo.dto;
+
+import com.example.demo.entity.Product;
+import com.example.demo.entity.Supplier;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Data
+public class ProductDTO {
+    private Long productId;
+    private String firstname;
+    private String lastname;
+    private int quantity;
+    private double price;
+    private String productName;
+    private String supplierName;
+
+    public ProductDTO(Product productEntity) {
+        this.productId = productEntity.getProductId();
+        this.firstname = productEntity.getFirstname();
+        this.lastname = productEntity.getLastname();
+        this.quantity = productEntity.getQuantity();
+        this.price = productEntity.getPrice();
+    }
+}
---
+++ src/main/java/com/example/demo/dto/SupplierDTO.java
@@ -0,0 +1,30 @@
+package com.example.demo.dto;
+
+import com.example.demo.entity.Supplier;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Data
+public class SupplierDTO {
+    private Long id;
+    private String name;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String address;
+    private Date dateOfBirth;
+    private String phoneNumber;
+
+    public SupplierDTO(Supplier supplierEntity) {
+        this.id = supplierEntity.getId();
+        this.firstname = supplierEntity.getFirstname();
+        this.lastname = supplierEntity.getLastname();
+        this.name = supplierEntity.getFirstname() + " " + supplierEntity.getLastname();
+        this.dateOfBirth = supplierEntity.getDateOfBirth();
+        this.email = supplierEntity.getEmail();
+        this.phoneNumber = supplierEntity.getPhoneNumber();
+        this.address = supplierEntity.getAddress();
+    }
+}
