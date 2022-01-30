3a64cb3d99a08fa07f5615a542588c79c237b8c7
HangTo <tominhhang1996@gmail.com>
2022-01-30 19:34:27 +0700

    Implement CacheService

--- src/main/java/com/example/demo/cache/CacheService.java
+++ src/main/java/com/example/demo/cache/CacheService.java
@@ -1,45 +1,71 @@
 package com.example.demo.cache;
 
-import com.example.demo.entity.Product;
 import com.example.demo.entity.Supplier;
-import com.vaadin.flow.component.Key;
+import net.sf.ehcache.search.Attribute;
+import net.sf.ehcache.search.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
+import org.springframework.beans.factory.annotation.Configurable;
+import net.sf.ehcache.Element;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CacheService {
+    private static final String CRON_TAB_EVERY_MID_NIGHT = "0 0 0 * * *";
+
     private final Logger log = LoggerFactory.getLogger(CacheService.class);
 
     @Autowired
+    private CacheConfig cacheConfig;
+
     private CacheManager cacheManager;
 
-    @Scheduled(cron = "0 0 0/1 * * ?")
+    public CacheService(CacheConfig cacheConfig) {
+        this.cacheConfig = cacheConfig;
+        this.cacheManager = cacheConfig.getCacheManager();
+    }
+
+    /**
+     * Cleans the entire cache
+     */
+    @Scheduled(cron = CRON_TAB_EVERY_MID_NIGHT)
     private void refreshCache() {
-        cacheManager.getCacheNames().stream()
-                    .forEach(cacheName -> cacheManager.getCache(cacheName).clear());
+//        cacheManager.getCacheNames().stream()
+//                    .forEach(cacheName -> cacheManager.getCache(cacheName).clear());
+        cacheManager.clearAll();
     }
-
-    public void addProductToCache(Product product) {
-        Cache cache = cacheManager.getCache("productCache");
-        cache.putIfAbsent(product.getId(), product);
-    }
-
-    public void addSupplierToCache(Supplier supplier) {
-        Cache cache = cacheManager.getCache("supplierCache");
-        cache.putIfAbsent(supplier.getId(), supplier);
-    }
-
-    // TODO: getEntryByKey
 //
-//    public void getEntryByKey(String cacheName, Key key) {
-//        log.info("Get entry from " + cacheName + " by key :" + key);
-//        Object result = null;
-//        Cache cache = cacheManager.getCache("supplierCache");
-//        cache.putIfAbsent(supplier.getId(), supplier);
+//    public void addProductToCache(Product product) {
+//        Cache cache = cacheManager.getCache("productCache");
+//        cache.putIfAbsent(product.getId(), product);
 //    }
+//
+
+    public List<Supplier> getAllSuppliersFromCache() {
+        List<Supplier> supplierList = cacheConfig.getAllSuppliersFromCache();
+        return supplierList;
+    }
+
+    public Supplier getSupplierByKeyFromCache(Long id) {
+        Cache supplierCache = cacheManager.getCache(CacheName.SUPPLIER_CACHE);
+        Element element = supplierCache.get(id);
+        return element != null ? (Supplier) element.getObjectValue() : null;
+    }
+
+    public String reloadSupplierCache(String action, Long id) {
+        String message = cacheConfig.reloadSupplierCache(action, id);
+        return message;
+    }
+
+    public String reloadProductCache(String action, Long id) {
+        String message = cacheConfig.reloadProductCache(action, id);
+        return message;
+    }
 }
