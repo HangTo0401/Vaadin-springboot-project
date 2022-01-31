319457d4243624334c7fdf3a5dcc5702d15d7098
HangTo <tominhhang1996@gmail.com>
2022-01-31 17:41:39 +0700

    Add cacheName file

--- src/main/java/com/example/demo/cache/CacheName.java
+++ src/main/java/com/example/demo/cache/CacheName.java
@@ -1,2 +1,10 @@
-package com.example.demo.cache;public class CacheName {
+package com.example.demo.cache;
+
+/**
+ * Each name must sync with src/main/resources/ehcache.xml
+ */
+public class CacheName {
+    public static final String SUPPLIER_CACHE = "supplierCache";
+
+    public static final String PRODUCT_CACHE = "productCache";
 }
