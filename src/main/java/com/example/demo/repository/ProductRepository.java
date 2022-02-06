caf710b0d2aea174c1538c3e632a82c5e714cf9f
HangTo <tominhhang1996@gmail.com>
2022-02-06 11:09:53 +0700

    Refractor repos

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,12 +1,16 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+
 import java.util.List;
 
 // This will be AUTO IMPLEMENTED CRUD by Spring into a Bean called ProductRepository
--- src/main/java/com/example/demo/repository/SupplierRepository.java
+++ src/main/java/com/example/demo/repository/SupplierRepository.java
@@ -1,20 +1,22 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Product;
 import com.example.demo.entity.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+
 import java.util.List;
 
 // This will be AUTO IMPLEMENTED CRUD by Spring into a Bean called SupplierRepository
 @Repository
 @Transactional
 public interface SupplierRepository extends JpaRepository <Supplier, Long> {
-    List<Supplier> findByNameStartsWithIgnoreCase(String name);
     @Query("select s from Supplier s " +
             "where lower(s.firstname) like lower(concat('%', :searchTerm, '%')) " +
             "or lower(s.lastname) like lower(concat('%', :searchTerm, '%'))")
